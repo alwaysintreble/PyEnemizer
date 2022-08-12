a870d48ad3b6ec1f74dfbf806dfcac10689a5451
alwaysintreble <mmmcheese158@gmail.com>
2022-08-12 01:34:24 -0500

    define some rooms with special rules

---
+++ src/SpecialRoomRules.py
@@ -0,0 +1,13 @@
+from DungeonRoomConstants import RoomConstants
+
+
+DONT_USE_IMMOVABLE_SPRITES: set = {
+    RoomConstants.palace_of_darkness_turtle_room
+}
+
+
+DONT_USE_FLYING_SPRITES: set = {
+    RoomConstants.misery_mire_mire_02_wizzrobes,
+    RoomConstants.mimic_cave
+}
+
